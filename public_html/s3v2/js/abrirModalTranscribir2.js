762b0578c741cb1867f39f0d5b105108ba2eec21
Jimmy Backend <jimmybackend@gmail.com>
2026-03-24 13:50:05 -0600

    Delete public_html/s3v2 directory
    el directorio se modifico por la version corregida de nombre s3-servicios-aws

--- public_html/s3v2/js/abrirModalTranscribir2.js
+++
@@ -1,8 +0,0 @@
-window.abrirModalTranscribir = function (key, nombre) {
-  $('#txArchivoKey').val(key);
-  $('#txLanguage').val('es-ES'); // default español
-  $('#txResultado').val('');
-  $('#txResultadoBox').addClass('d-none');
-  $('#txCargando').addClass('d-none');
-  $('#modalTranscribir').modal('show');
-};

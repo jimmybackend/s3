964bf48400fafc41f7bd134fb6012d01e120213a
Jimmy Backend <jimmybackend@gmail.com>
2026-09-05 10:15:21 -0600

    Move folder listing endpoint behind query controller

--- drive/listar_carpetas.php
+++ drive/listar_carpetas.php
@@ -1,27 +1,9 @@
 <?php
+declare(strict_types=1);
+
 require_once __DIR__ . '/app_bootstrap.php';
-require_once __DIR__ . '/S3Manager.php';
 
-header('Content-Type: application/json; charset=UTF-8');
-
-try {
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
-
-    $manager = new S3Manager();
-
-    $carpetas = $manager->listarCarpetasDesdeDb();
-
-    echo json_encode([
-        'ok' => true,
-        'carpetas' => $carpetas
-    ], JSON_UNESCAPED_UNICODE);
-
-} catch (Throwable $e) {
-    http_response_code(500);
-    echo json_encode([
-        'ok'    => false,
-        'error' => $e->getMessage()
-    ], JSON_UNESCAPED_UNICODE);
-}
+(new \ArcadeCloud\Drive\Http\Controller\FolderQueryController(
+    \ArcadeCloud\Drive\Core\ApplicationKernel::app(),
+    \ArcadeCloud\Drive\Http\Request::fromGlobals()
+))->list();

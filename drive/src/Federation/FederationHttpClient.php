97ad4d41b1d14b1254bda8da2b45dddd06d621e6
Jimmy Backend <jimmybackend@gmail.com>
2026-09-11 16:43:13 -0600

    feat: allow FederationCloud replica offers

--- drive/src/Federation/FederationHttpClient.php
+++ drive/src/Federation/FederationHttpClient.php
@@ -21,6 +21,7 @@ final class FederationHttpClient
         'sync-push.php',
         'access-request.php',
         'access-status.php',
+        'replica-offer.php',
     ];
 
     public function getJson(string $federationUrl, string $endpoint): array

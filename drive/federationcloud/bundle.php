486f5f6aac0f7d748492f932311eb8633f1020b9
Jimmy Backend <jimmybackend@gmail.com>
2026-09-12 21:12:23 -0600

    refactor: route bulk ArcadeLinks through dedicated controller

--- drive/federationcloud/bundle.php
+++ drive/federationcloud/bundle.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 require_once dirname(__DIR__) . '/app_bootstrap.php';
 
 use ArcadeCloud\Drive\Core\ApplicationKernel;
-use ArcadeCloud\Drive\Http\Controller\FederationController;
+use ArcadeCloud\Drive\Http\Controller\FederationBundleController;
 use ArcadeCloud\Drive\Http\Request;
-use ArcadeCloud\Drive\View\FederationPageRenderer;
 
-$controller = new FederationController(ApplicationKernel::app(), Request::fromGlobals(), new FederationPageRenderer());
-$controller->bundleApi();
+$controller = new FederationBundleController(ApplicationKernel::app(), Request::fromGlobals());
+$controller->download();

d6901655f887e4e1f3748e3054d4920ba379f4df
Jimmy Backend <jimmybackend@gmail.com>
2026-09-12 21:10:46 -0600

    feat: add bulk ArcadeLink bundle endpoint

---
+++ drive/federationcloud/bundle.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/app_bootstrap.php';
+
+use ArcadeCloud\Drive\Core\ApplicationKernel;
+use ArcadeCloud\Drive\Http\Controller\FederationController;
+use ArcadeCloud\Drive\Http\Request;
+use ArcadeCloud\Drive\View\FederationPageRenderer;
+
+$controller = new FederationController(ApplicationKernel::app(), Request::fromGlobals(), new FederationPageRenderer());
+$controller->bundleApi();

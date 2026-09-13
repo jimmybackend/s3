829f25f9b8f1cee3747e84eba64636f4a46e1e6c
Jimmy Backend <jimmybackend@gmail.com>
2026-09-12 21:13:42 -0600

    chore: keep direct download endpoint on feature branch

--- drive/download.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/app_bootstrap.php';
-(new \ArcadeCloud\Drive\Http\Controller\FileAccessController(
-    \ArcadeCloud\Drive\Core\ApplicationKernel::app(),
-    \ArcadeCloud\Drive\Http\Request::fromGlobals()
-))->signedDownload();

3c77e0c4e26b757eb412ecd7420e8a681a249c7d
Jimmy Backend <jimmybackend@gmail.com>
2026-09-17 00:06:25 -0600

    feat: add folder document creation endpoint

---
+++ drive/create_folder_document.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/app_bootstrap.php';
+
+(new \ArcadeCloud\Drive\Http\Controller\FolderDocumentController(
+    \ArcadeCloud\Drive\Core\ApplicationKernel::app(),
+    \ArcadeCloud\Drive\Http\Request::fromGlobals()
+))->create();

8fb8752d20918f74e18b8d147047ee9cbb4d0ac8
Jimmy Backend <jimmybackend@gmail.com>
2026-09-24 15:24:10 -0600

    feat(media): expose EC2 worker status to processing modal

--- drive/src/Http/Controller/MediaProcessingController.php
+++ drive/src/Http/Controller/MediaProcessingController.php
@@ -6,6 +6,7 @@ namespace ArcadeCloud\Drive\Http\Controller;
 use ArcadeCloud\Drive\Http\JsonResponse;
 use ArcadeCloud\Drive\Media\MediaProcessingJobRepository;
 use ArcadeCloud\Drive\Media\MediaProcessingService;
+use ArcadeCloud\Drive\Media\MediaWorkerNodeService;
 
 final class MediaProcessingController extends AbstractJsonController
 {
@@ -14,14 +15,21 @@ final class MediaProcessingController extends AbstractJsonController
         try {
             $userId = $this->guardAuthenticated();
             $jobs = new MediaProcessingJobRepository($this->app->db());
-            $service = new MediaProcessingService($this->app->fileRecordLocator(), $jobs);
+            $node = new MediaWorkerNodeService($this->app->db());
+            $service = new MediaProcessingService($this->app->fileRecordLocator(), $jobs, $node);
 
             if ($this->request->method() === 'POST') {
                 $result = $service->enqueue($userId, $this->request->allPost());
                 JsonResponse::send($result, 202);
             }
 
-            JsonResponse::send($service->recent($userId));
+            if ($this->request->queryString('node_status') === '1') {
+                JsonResponse::send(['ok' => true, 'node' => $node->status()]);
+            }
+
+            $recent = $service->recent($userId);
+            $recent['node'] = $node->status();
+            JsonResponse::send($recent);
         } catch (\Throwable $e) {
             $this->fail($e, 400);
         }
